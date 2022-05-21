4fa13a58791939701688fd285696033d9767f2e1
reidisbot <101140697+reidisbot@users.noreply.github.com>
2022-05-20 20:45:41 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include "src/raylib.h"
+
+int main() 
+{
+    Color white = {255, 255, 255, 255};
+    Color grey = {211, 211, 211, 211};
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+    
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+            ClearBackground(white);
+            DrawText("Hello World!", 190, 200, 40, grey);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+    
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+    
+    return 0;
+}
